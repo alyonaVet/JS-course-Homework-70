c71a719ec2e80f32d359fa70ec453ec45acf1825
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-22 15:45:31 +0300

    added state contacts;

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {contactsReducer} from '../features/contacts/contactsSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    contacts: contactsReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
