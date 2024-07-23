125dab75e4dd0de52b7cfb4617d003af915a6328
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-23 14:40:59 +0300

    refactor App; added Route edit-contact;

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import NewContact from './containers/NewContact/NewContact';
 import { Route, Routes} from 'react-router-dom';
 import MainPage from './containers/MainPage/MainPage';
+import EditContact from './containers/EditContact/EditContact';
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/new-contact" element={<NewContact />} />
+          <Route path="/edit-contact/:id" element={<EditContact />}/>
           <Route path="*" element={<h3 className="mt-3 mb-5 text-center">Page not found</h3>} />
         </Routes>
       </main>
