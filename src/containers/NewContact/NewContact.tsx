f4ef2f284494943a3680d1d3cb7473736b33db87
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-23 14:39:01 +0300

    refactor NewContact;

--- src/containers/NewContact/NewContact.tsx
+++ src/containers/NewContact/NewContact.tsx
@@ -1,11 +1,10 @@
 import AddContactForm from '../../features/contacts/components/AddContactForm';
-import {useAppDispatch, useAppSelector} from '../../app/hooks';
-import {addContact,  selectCreateContactLoading} from '../../features/contacts/contactsSlice';
+import {useAppDispatch} from '../../app/hooks';
+import {addContact} from '../../features/contacts/contactsSlice';
 import {ContactFormType} from '../../types';
 
 const NewContact = () => {
   const dispatch = useAppDispatch();
-  const isCreating = useAppSelector(selectCreateContactLoading);
 
   const onSubmit = async (contact: ContactFormType) => {
     try {
@@ -17,7 +16,7 @@ const NewContact = () => {
 
   return (
     <div className="container mt-5">
-      <AddContactForm onSubmit={onSubmit} isSaving={isCreating} />
+      <AddContactForm onSubmit={onSubmit} />
     </div>
   );
 };
