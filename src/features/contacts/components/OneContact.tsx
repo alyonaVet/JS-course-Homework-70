36a77655f761d7d40160bc33378b44b2a329cdaa
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-23 14:40:05 +0300

    refactor OneContact; added isDeleting props;

--- src/features/contacts/components/OneContact.tsx
+++ src/features/contacts/components/OneContact.tsx
@@ -6,19 +6,21 @@ interface OneContactProps {
   onClose: () => void;
   onEdit: () => void;
   onDelete: () => void;
+  isDeleting: boolean;
 }
 
 
-const OneContact: React.FC<OneContactProps> = ({ contact, onClose, onEdit, onDelete}) => {
+const OneContact: React.FC<OneContactProps> = ({contact, onClose, onEdit, onDelete, isDeleting}) => {
+
   return (
     <>
       <div
         className={`modal-backdrop show`}
-        style={{ display: 'block' }}
+        style={{display: 'block'}}
       />
       <div
         className={`modal show`}
-        style={{ display: 'block' }}
+        style={{display: 'block'}}
         onClick={onClose}
       >
         <div
@@ -27,18 +29,40 @@ const OneContact: React.FC<OneContactProps> = ({ contact, onClose, onEdit, onDel
         >
           <div className="modal-content">
             <div className="modal-header">
-              <h1 className="modal-title fs-5">{contact.name}</h1>
               <button type="button" className="btn-close" aria-label="Close" onClick={onClose}></button>
             </div>
-            <div className="modal-body">
-              {contact.image && <img src={contact.image} alt="Contact" className="img-fluid" />}
-              <p><strong>Name:</strong> {contact.name}</p>
-              <p><strong>Phone:</strong> {contact.phone}</p>
-              <p><strong>Email:</strong> {contact.email}</p>
+            <div className="modal-body d-flex">
+              <div>
+                {contact.image ?
+                  <img src={contact.image} alt="Contact" className="img-fluid "
+                       style={{width: '150px', height: 'auto'}}/>
+                  : (
+                    <div className="py-5 px-4 border border-1">No photo</div>
+                  )
+                }
+              </div>
+              <div className="ms-4">
+                <h1 className="modal-title fs-5 mb-3">{contact.name}</h1>
+                <span className="d-block mb-2"><strong>Phone: </strong><a href="#">{contact.phone}</a></span>
+                <span className="d-block"><strong>Email: </strong><a href="#">{contact.email}</a></span>
+              </div>
             </div>
             <div className="modal-footer">
-              <button type="button" className="btn btn-primary" onClick={onEdit}>Edit</button>
-              <button type="button" className="btn btn-danger" onClick={onDelete}>Delete</button>
+              <button
+                type="button"
+                className="btn btn-secondary px-4"
+                onClick={onEdit}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={onDelete}
+                disabled={isDeleting}
+              >
+                {isDeleting ? 'Deleting...' : 'Delete'}
+              </button>
             </div>
           </div>
         </div>
