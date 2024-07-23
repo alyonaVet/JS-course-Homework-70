a8ffc9225381ee9e42f1d490677926d352c2851d
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-23 12:30:00 +0300

    refactor: added onClick;

--- src/components/Contacts/ContactItem.tsx
+++ src/components/Contacts/ContactItem.tsx
@@ -2,24 +2,22 @@ import React from 'react';
 import {ContactType} from '../../types';
 
 interface ContactItemProps {
-  contact: ContactType;
+  contact: ContactType | null;
+  onClick: React.MouseEventHandler;
 }
 
-const ContactItem: React.FC<ContactItemProps> = ({contact}) => {
+const ContactItem: React.FC<ContactItemProps> = ({contact, onClick}) => {
   return (
-    <div className="container d-flex flex-column mt-5">
-      <div className="card mb-3 p-3" style={{maxWidth: '540px'}}>
-        <div className="row g-0">
+      <div className="card mb-3 p-3" style={{maxWidth: '540px', height: '165px'}} onClick={onClick}>
+        <div className=" d-flex g-0">
           <div className="col-md-4">
-            <img src={contact.image} className="img-fluid rounded-start" alt=""/>
+            <img src={contact?.image} className="img-fluid rounded" alt="Photo"/>
           </div>
-          <div className="col-md-8">
-            <h4 className="card-title">{contact.name}</h4>
+          <div className="col-md-8 align-self-center">
+            <h4 className="card-title ms-5">{contact?.name}</h4>
           </div>
         </div>
       </div>
-    </div>
-
   );
 };
 
