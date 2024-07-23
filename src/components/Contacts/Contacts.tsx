c7c97184e6b81f151f839565ea9772a36bd06cbc
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-23 11:54:07 +0300

    refactor Contacts; added show one contact;

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,24 +1,41 @@
 import ContactItem from './ContactItem';
 import {useAppSelector} from '../../app/hooks';
 import {selectContacts} from '../../features/contacts/contactsSlice';
+import {useState} from 'react';
+import {ContactType} from '../../types';
+import OneContact from '../../features/contacts/components/OneContact';
+
 
 const Contacts = () => {
+  const [showContact, setShowContact] = useState<ContactType | null>(null);
   const contacts = useAppSelector(selectContacts);
-  console.log(contacts);
 
   return (
-    <div>
-      {contacts.length > 0 ? (
-        contacts.map((contact) => (
-          <ContactItem
-            key={contact.id}
-            contact={contact}
+    <>
+      <div className="container d-flex flex-column mt-4">
+        {contacts.length > 0 ? (
+          contacts.map((contact) => (
+            <ContactItem
+              key={contact.id}
+              contact={contact}
+              onClick={() => setShowContact(contact)}
+            />
+          ))
+        ) : (
+          <div className="align-self-center mt-5">No contacts added yet</div>
+        )}
+      </div>
+      {
+        showContact !== null && (
+          <OneContact
+            contact={showContact}
+            onClose={() => setShowContact(null)}
+            onEdit={() => null}
+            onDelete={() => null}
           />
-        ))
-      ) : (
-        'No contacts added yet'
-      )}
-    </div>
+        )
+      }
+    </>
   );
 };
 
