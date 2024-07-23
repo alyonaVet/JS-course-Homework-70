769c44cff8159620f36218e50f2108ac4e44de5a
alyonaVet <a.v.alenka2015@gmail.com>
2024-07-23 14:04:31 +0300

    refactor styles;

--- src/components/Contacts/ContactItem.tsx
+++ src/components/Contacts/ContactItem.tsx
@@ -8,16 +8,21 @@ interface ContactItemProps {
 
 const ContactItem: React.FC<ContactItemProps> = ({contact, onClick}) => {
   return (
-      <div className="card mb-3 p-3" style={{maxWidth: '540px', height: '165px'}} onClick={onClick}>
-        <div className=" d-flex g-0">
-          <div className="col-md-4">
-            <img src={contact?.image} className="img-fluid rounded" alt="Photo"/>
-          </div>
-          <div className="col-md-8 align-self-center">
-            <h4 className="card-title ms-5">{contact?.name}</h4>
-          </div>
+    <div className="card mb-3 p-3" style={{maxWidth: '540px', height: '165px'}} onClick={onClick}>
+      <div className="row g-0">
+        <div className="col-4">
+          {contact?.image ?
+            <img src={contact.image} alt="Contact photo" className="" style={{width: '150px', height: 'auto'}}/>
+            : (
+              <div className="py-5 px-4 border border-1 text-center">No photo</div>
+            )
+          }
+        </div>
+        <div className="col-8 d-flex align-items-center justify-content-between">
+          <h4 className="card-title ms-5">{contact?.name}</h4>
         </div>
       </div>
+    </div>
   );
 };
 
